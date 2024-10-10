51c3deb755d3d2f2cdb6de1c7931b8f8c2a878cf
Ivan Kleshnin <ivan@paqmind.com>
2024-10-10 12:58:29 +0300

    Cleanup

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,27 +1,27 @@
-from .category import Categorizer
-from .phone import parse_phones
-from .email import parse_emails
-from .freelancer import FreelancerParser
-from .language import detect_language_iso639
-from .nondev import NondevParser
-from .student import StudentParser
-from .web import html2text, markdown2text
-
-__all__ = [
-  # Phone/Email parsers
-  "parse_phones",
-  "parse_emails",
-
-  # Human language parser
-  "detect_language_iso639",
-
-  # Categorizers
-  "Categorizer",
-  "FreelancerParser",
-  "NondevParser",
-  "StudentParser",
-
-  # Web Utils
-  "html2text",
-  "markdown2text"
-]
+# from .category import Categorizer
+# from .phone import parse_phones
+# from .email import parse_emails
+# from .freelancer import FreelancerParser
+# from .language import detect_language_iso639
+# from .nondev import NondevParser
+# from .student import StudentParser
+# from .web import html2text, markdown2text
+#
+# __all__ = [
+#   # Phone/Email parsers
+#   "parse_phones",
+#   "parse_emails",
+#
+#   # Human language parser
+#   "detect_language_iso639",
+#
+#   # Categorizers
+#   "Categorizer",
+#   "FreelancerParser",
+#   "NondevParser",
+#   "StudentParser",
+#
+#   # Web Utils
+#   "html2text",
+#   "markdown2text"
+# ]
